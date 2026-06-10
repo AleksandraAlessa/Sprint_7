af6db84a662106b55d4e951e2653147688e40a16
Aleksandra Usova <f.alessa@yandex.ru>
2026-06-10 09:59:39 +0300

    внесено удаление тестовых данных

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -1,86 +1,49 @@
 import allure
 import pytest
 import requests
-from helpers.courier_helpers import generate_random_string
+from data import BASE_URL
 
 @allure.feature("Логин курьера")
 class TestLoginCourier:
 
     @allure.title("Успешная авторизация курьера, возвращается id")
-    def test_login_success(self, base_url):
-        # 1. Создаём курьера
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-        create_payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        }
-        create_response = requests.post(f"{base_url}/api/v1/courier", data=create_payload)
-        assert create_response.status_code == 201
-
-        # 2. Пытаемся авторизоваться
+    def test_login_success(self, created_courier):
         login_payload = {
-            "login": login,
-            "password": password
+            "login": created_courier["login"],
+            "password": created_courier["password"]
         }
-        response = requests.post(f"{base_url}/api/v1/courier/login", data=login_payload)
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", data=login_payload)
         assert response.status_code == 200
         assert "id" in response.json()
         assert isinstance(response.json()["id"], int)
+        # Дополнительно проверим, что id совпадает с id созданного курьера
+        assert response.json()["id"] == created_courier["id"]
 
     @allure.title("Ошибка при авторизации: неправильный логин")
-    def test_login_wrong_login(self, base_url):
-        # 1. Создаём курьера
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-        create_payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        }
-        create_response = requests.post(f"{base_url}/api/v1/courier", data=create_payload)
-        assert create_response.status_code == 201
-
-        # 2. Пробуем войти с неправильным логином
+    def test_login_wrong_login(self, created_courier):
         wrong_login_payload = {
-            "login": login + "wrong",
-            "password": password
+            "login": created_courier["login"] + "wrong",
+            "password": created_courier["password"]
         }
-        response = requests.post(f"{base_url}/api/v1/courier/login", data=wrong_login_payload)
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", data=wrong_login_payload)
         assert response.status_code == 404
         assert "Учетная запись не найдена" in response.text
 
     @allure.title("Ошибка при авторизации: неправильный пароль")
-    def test_login_wrong_password(self, base_url):
-        # 1. Создаём курьера
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-        create_payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        }
-        create_response = requests.post(f"{base_url}/api/v1/courier", data=create_payload)
-        assert create_response.status_code == 201
-
-        # 2. Пробуем войти с неправильным паролем
+    def test_login_wrong_password(self, created_courier):
         wrong_password_payload = {
-            "login": login,
-            "password": password + "wrong"
+            "login": created_courier["login"],
+            "password": created_courier["password"] + "wrong"
         }
-        response = requests.post(f"{base_url}/api/v1/courier/login", data=wrong_password_payload)
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", data=wrong_password_payload)
         assert response.status_code == 404
         assert "Учетная запись не найдена" in response.text
 
     @allure.title("Ошибка при авторизации несуществующего пользователя")
-    def test_login_non_existent_user(self, base_url):
+    def test_login_non_existent_user(self, base_url, courier_data):
         payload = {
-            "login": generate_random_string(10),
-            "password": generate_random_string(10)
+            "login": courier_data["login"],
+            "password": courier_data["password"]
         }
         response = requests.post(f"{base_url}/api/v1/courier/login", data=payload)
         assert response.status_code == 404
@@ -88,13 +51,9 @@ class TestLoginCourier:
 
     @allure.title("Ошибка: Недостаточно данных для входа")
     @pytest.mark.parametrize("missing_field", ["login", "password"])
-    def test_login_missing_field(self, base_url, missing_field):
-        # Генерируем данные, но затем удаляем одно поле
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        payload = {"login": login, "password": password}
+    def test_login_missing_field(self, base_url, courier_data, missing_field):
+        payload = courier_data.copy()
         del payload[missing_field]
         response = requests.post(f"{base_url}/api/v1/courier/login", data=payload)
         assert response.status_code == 400
-        # Ожидаемый текст ошибки 
-        assert "Недостаточно данных для входа" in response.text 
+        assert "Недостаточно данных для входа" in response.text
